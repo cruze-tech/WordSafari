d32ffe3207baedf46bcaa69cc729a58a71d6f0ae
Gabriel <gabparsley@gmail.com>
2026-02-25 10:55:01 +0300

    feat: Implement PWA capabilities with a service worker and web app manifest.

---
+++ sw.js
@@ -0,0 +1,76 @@
+const CACHE_NAME = 'wordsafari-cache-v2';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './css/style.css',
+    './js/data.js',
+    './js/audio.js',
+    './js/achievements.js',
+    './js/progression.js',
+    './js/game.js',
+    './js/ui.js',
+    './manifest.json'
+];
+
+// Install Event
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+    self.skipWaiting();
+});
+
+// Activate Event
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('Clearing old cache');
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+// Fetch Event
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response; // Return cached version
+                }
+
+                return fetch(event.request).then(
+                    function (response) {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // IMPORTANT: Clone the response. A response is a stream
+                        // and because we want the browser to consume the response
+                        // as well as the cache consuming the response, we need
+                        // to clone it so we have two streams.
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function (cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
